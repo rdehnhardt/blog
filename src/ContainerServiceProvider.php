a5d90bf0fd9a3063d02d61e9a5216b66da8fb36c
vagrant <vagrant@homestead>
2019-09-09 19:21:41 +0000

    First Commit

---
+++ src/ContainerServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Skeleton\Container;
+
+use Skeleton\Support\SkeletonServiceProvider;
+
+class ContainerServiceProvider extends SkeletonServiceProvider
+{
+    /**
+     * @var string
+     */
+    protected $name = 'container';
+
+    /**
+     * @var string
+     */
+    protected $directory = __DIR__;
+
+    /**
+     * @var string
+     */
+    protected $namespace = __NAMESPACE__;
+}

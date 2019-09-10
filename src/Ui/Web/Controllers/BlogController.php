44a8eee9ce5081a2aab116c5a93ed17ad424dffd
rdehnhardt <renatotkd@gmail.com>
2019-09-10 13:29:14 +0200

    Add parents

--- src/Ui/Web/Controllers/BlogController.php
+++ src/Ui/Web/Controllers/BlogController.php
@@ -8,6 +8,6 @@ class BlogController extends Controller
 {
     public function index()
     {
-        return view('container::index');
+        return view('blog::index');
     }
 }
--- src/Ui/Web/Views/index.blade.php
+++ src/Ui/Web/Views/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Container</title>
+    <title>Blog</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -67,7 +67,7 @@
 <div class="flex-center position-ref full-height">
 
     <div class="content">
-        <div class="title m-b-md">Container</div>
+        <div class="title m-b-md">Blog</div>
 
         <div class="links">
             <a href="https://laravel.com/docs">Docs</a>
